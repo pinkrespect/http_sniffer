73005a56d907ae9e7b049df5c4922cdc10ad729e
Didee <miss.sonagi@gmail.com>
2017-09-28 01:22:38 +0900

    Edit if state syntax for readability and some syntax.

--- project.py
+++ project.py
@@ -6,53 +6,53 @@ import sys
 
 
 def payload_parser(payload, data_file, idx):
+    eth_length = 14
+    eth_protocol = socket.ntohs(unpack('!6s6sH', payload[:eth_length])[2])
+
+    if not eth_protocol == 8:
+        return idx
+
+    ip_header = unpack('!BBHHHBBH4s4s', payload[eth_length:20+eth_length])
+    version_ih = ip_header[0]
+    ip_header_length = (version_ih & 0xF) * 4
+    protocol = ip_header[6]
+    source_address = socket.inet_ntoa(ip_header[8])
+    dest_address = socket.inet_ntoa(ip_header[9])
+
+    if not protocol == 6:
+        return idx
+
+    tcp_header_index = ip_header_length + eth_length
+    tcp_header = unpack('!HHLLBBHHH',
+                        payload[tcp_header_index:tcp_header_index+20])
+
+    source_port = tcp_header[0]
+    dest_port = tcp_header[1]
+    data_offset_reserved = tcp_header[4]
+    tcp_header_length = data_offset_reserved >> 4
+
+    tcp_header_size = eth_length + ip_header_length + tcp_header_length * 4
+    data_list = payload[tcp_header_size:].partition(b'\r\n\r\n')[0]
+
     try:
-        eth_length = 14
-        eth = unpack('!6s6sH', payload[:eth_length])
-        eth_protocol = socket.ntohs(eth[2])
-        if eth_protocol == 8:
-            ip_header = payload[eth_length:20+eth_length]
-            ip_header = unpack('!BBHHHBBH4s4s', ip_header)
+        data_splited = str(data_list, 'utf-8').split('\r\n')
 
-            version_ih = ip_header[0]
-            ip_header_length = (version_ih & 0xF) * 4
-            protocol = ip_header[6]
-            source_address = socket.inet_ntoa(ip_header[8])
-            dest_address = socket.inet_ntoa(ip_header[9])
+        if "HTTP/1.1" not in data_splited[0]:
+            return idx
 
-            if protocol == 6:
-                tcp_header_index = ip_header_length + eth_length
-                tcp_header = payload[tcp_header_index:tcp_header_index+20]
-                tcp_header = unpack('!HHLLBBHHH', tcp_header)
+        data_file.write("#" + str(idx) + " " + str(source_address) + ":" +
+                        str(source_port) + " " + str(dest_address) + ":" +
+                        str(dest_port) + "\n")
+        idx = idx + 1
 
-                source_port = tcp_header[0]
-                dest_port = tcp_header[1]
-                data_offset_reserved = tcp_header[4]
-                tcp_header_length = data_offset_reserved >> 4
+        for string in data_splited:
+            data_file.write(string + "\n")
 
-                header_size = eth_length + ip_header_length +\
-                    tcp_header_length * 4
-
-                data = payload[header_size:]
-                data_list = data.partition(b'\r\n\r\n')[0]
-                
-                data_splited = str(data_list, 'utf-8').split('\r\n')
-                if "HTTP/1.1" in data_splited[0]:
-                    data_file.write("#" + str(idx) + " " +
-                                    str(source_address) + ":" +
-                                    str(source_port) + " " +
-                                    str(dest_address) + ":" +
-                                    str(dest_port) + "\n")
-                    idx = idx + 1
-                    for string in data_splited:
-                        data_file.write(string + "\n")
-
-                    data_file.write("\n")
+        data_file.write("\n")
+        return idx
 
     except UnicodeDecodeError:
-        pass
-
-    return idx
+        return idx
 
 
 def main(argv):
@@ -60,21 +60,26 @@ def main(argv):
     devices = os.listdir('/sys/class/net/')
 
     for index, device in enumerate(devices):
-        print("{0} {1}".format(index+1, device))
-
-    device_name = devices[int(input())-1]
-    captured = pcapy.open_live(device_name, 65536, 1, 1)
-    idx = 0
-
+        print(str(index+1) + " " + str(device))
+    try:
+        captured = pcapy.open_live(devices[int(input())-1], 65536, 1, 1)
+    except pcapy.PcapError:
+        print("Permission Denied" +
+              "You don't have permission to capture on that device " +
+              "(socket: Operation not permitted)")
+        return 1
     (header, packet) = captured.next()
     data_file = open("./result.txt", "w")
+
     try:
+        idx = 0
         while header is not None:
             (header, packet) = captured.next()
             idx = payload_parser(packet, data_file, idx)
+
     except KeyboardInterrupt:
         data_file.close()
-        pass
+        return 0
 
 
 if __name__ == "__main__":
